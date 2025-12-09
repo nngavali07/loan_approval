784a0924d38c497ac164ff1d67cb54f93c81d6e4
Nikhil Gavali <nikhilngavali@gmail.com>
2025-12-10 00:47:47 +0530

    Updated files

--- main.py
+++ main.py
@@ -41,6 +41,3 @@ def predictions():
         return jsonify({"Result": "Loan Declined"})
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port, debug=False)
